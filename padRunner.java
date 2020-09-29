f02f06c6d037ea22c5137df0b11880770ad2ac64
Raymond <71736469+rwelgosh@users.noreply.github.com>
2020-09-29 18:52:29 -0400

    Created padRunner.java
    First creation of padRunner.java
    Fresh creation with a 600 by 600 window with nothing on it.

---
+++ padRunner.java
@@ -0,0 +1,13 @@
+import processing.core.PApplet;
+
+public class padRunner extends PApplet {
+
+	public void settings() {
+		size(600, 600);
+	}
+	
+	public static void main(String[] args) {
+		PApplet.main("padRunner");
+	}
+	
+}
