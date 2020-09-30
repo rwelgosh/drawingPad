7133b2b9ba7ca897b53acb965feb0aa7ae2bccfe
Raymond <71736469+rwelgosh@users.noreply.github.com>
2020-09-30 08:58:45 -0400

    Update padRunner.java
    Made it work with the file creating class that I made.

--- padRunner.java
+++ padRunner.java
@@ -2,8 +2,13 @@ import processing.core.PApplet;
 
 public class padRunner extends PApplet {
 
+	private static saveFile s;
+	private int[][] pad = new int[540][540];
+	private static int index = 0;
+	
 	public void settings() {
 		size(800, 900);
+		s = new saveFile();
 	}
 	
 	public void setup() {
@@ -23,7 +28,39 @@ public class padRunner extends PApplet {
 			System.out.println("pressed");
 			if((mouseX >= width/2 - 270 && mouseX <= width/2 + 270) && (mouseY >= height/2 - 370 & mouseY <= height/2 + 170)) {
 				rect(mouseX, mouseY, 20, 20);
+				for(int i = 0; i < 20; i++) {
+					for(int j = 0; j < 20; j++) {
+						pad[(mouseX-10) + i][(mouseY-10) + j] = 1;
+					}
+				}
 			}
+		}
+	}
+	
+	public void mouseReleased() {
+		if((mouseX >= width/2 - 100 && mouseX <= width/2 + 100) && (mouseY >= 775 && mouseY <= 825)) {
+			int[][] picture = new int[27][27];
+			for(int i = 0; i < 27; i++) {
+				for(int j = 0; j < 27; j++) {
+					
+					int sum = 0;
+					for(int k = 0; k < 20; k++) {
+						for(int l = 0; l < 20; l++) {
+							sum += pad[(i*20) + k][(j*20) + l];
+							pad[(i*20) + k][(j*20) + l] = 0;
+						}
+					}
+					picture[i][j] = sum;
+					
+				}
+			}
+			
+			s.matrixToFile(picture, index);
+			index++;
+			
+			fill(255);
+			rect(width/2, height/2 - 100, 540, 540);
+			
 		}
 	}
 	
